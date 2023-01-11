2c0d7a4a9b74dd2030854e6c3419b7b555c53e81
DOMEMY <konton07@hotmail.com>
2023-01-11 21:01:24 +0700

    Updated

--- vitalsenseapp/lib/main.dart
+++ vitalsenseapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,88 +14,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         appBarTheme: AppBarTheme().copyWith(
           backgroundColor: Color(0xFFFFF9F3),
         ),
         scaffoldBackgroundColor: Color(0xFFFFF9F3),
       ),
       home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset("images/Vector.png"),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              Padding(
-                padding: EdgeInsets.only(left: 40, right: 40, top: 20),
-                child: Text(
-                  "Email:",
-                  style: TextStyle(color: Colors.black, fontSize: 20),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 40, right: 40),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Email",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(50),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 40, right: 40, top: 10),
-                child: Text(
-                  "Password:",
-                  style: TextStyle(color: Colors.black, fontSize: 20),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 40, right: 40),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Password",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(50),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
     );
   }
 }
