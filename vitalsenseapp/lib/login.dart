a2e3fa83c4bc5474fb7c0c64869997254088e711
oomnk <oom-nichapat@hotmail.com>
2023-01-24 18:39:06 +0700

    fix button bug

--- vitalsenseapp/lib/login.dart
+++ vitalsenseapp/lib/login.dart
@@ -15,21 +15,23 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset("assets/images/Vector.png"),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              Padding(
-                padding: EdgeInsets.only(left: 40, right: 40, top: 20),
-                child: Text(
-                  "Email:",
-                  style: TextStyle(color: Colors.black, fontSize: 20),
+      body: Container(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children:[
+            Image.asset("assets/images/Vector.png"),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 40, right: 40, top: 20),
+                  child: Text(
+                    "Email:",
+                    style: TextStyle(color: Colors.black, fontSize: 20),
+                  ),
                 ),
-              ),
+
               Padding(
                 padding: EdgeInsets.only(left: 40, right: 40),
                 child: TextField(
@@ -65,29 +67,46 @@ class _LoginState extends State<Login> {
                   ),
                 ),
               ),
-              /*Align(
-                alignment: Alignment.bottomRight,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 40, right: 40, top: 30),
-                  child: FloatingActionButton(
-                    backgroundColor: Colors.amberAccent,
-                    child: Icon(
-                      Icons.start,
-                      size: 35,
-                      color: Colors.black,),
-                    onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(
-                          builder: (context){
-                        return MyWidget();
-                        })
-                      );
-                     },
+              Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 40, right: 40, top: 30),
+                    child: FloatingActionButton(
+                      backgroundColor: Colors.amberAccent,
+                      child: Icon(
+                        Icons.account_circle,
+                        size: 35,
+                        color: Colors.black,
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
                     ),
                   ),
-                ),*/
-            ],
-          ),
-        ],
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 40, right: 40, top: 30),
+                    child: FloatingActionButton(
+                      backgroundColor: Colors.amberAccent,
+                      child: Icon(
+                        Icons.login,
+                        size: 35,
+                        color: Colors.black,),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const MyWidget()));
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
