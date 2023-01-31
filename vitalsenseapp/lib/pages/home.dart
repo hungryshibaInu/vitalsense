b2469390b8ba6152e5662cb0feef01ef9ae74079
hungryshibamacair <prom.p2001@gmail.com>
2023-01-31 19:20:32 +0700

    addchangetohome

--- vitalsenseapp/lib/pages/home.dart
+++ vitalsenseapp/lib/pages/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:vitalsenseapp/homeCard.dart';
+import 'package:vitalsenseapp/pages/homeCard.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
